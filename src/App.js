33d9fe2775a195d8361404e10203986bfa4e7d7d
zillur-rgb <zillurdeu@gmail.com>
2022-05-21 23:03:53 +0200

    Daisy UI setup and theme customization

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl text-red-600">Hello Hello</h1>
+      <h1 className="text-3xl text-header font-header">Hello Hello</h1>
+      <h1 className="text-3xl p-60 text-header font-header">Hello Hello</h1>
+      <button class="btn bg-primary hover:bg-hover">Button</button>
     </div>
   );
 }
