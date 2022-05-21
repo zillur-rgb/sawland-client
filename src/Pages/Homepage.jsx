e80510543f08272f98986a925d7543c737ad7e5e
zillur-rgb <zillurdeu@gmail.com>
2022-05-22 00:31:30 +0200

    Statistics

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../Components/Homepage/Hero";
+import Statistics from "../Components/Shared/Statistics";
 
 const Homepage = () => {
   return (
     <div>
       <Hero />
+      <Statistics />
     </div>
   );
 };
