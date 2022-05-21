25c08b639716e999287198319b1b2369314d088c
zillur-rgb <zillurdeu@gmail.com>
2022-05-21 22:42:48 +0200

    Tailwind setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Hello</h1>
+      <h1 className="text-3xl text-red-600">Hello Hello</h1>
     </div>
   );
 }
